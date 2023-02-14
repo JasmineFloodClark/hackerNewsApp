12d4509413c4d0d30bae2396e09c4edd40124435
JasmineFloodClark <125389358+JasmineFloodClark@users.noreply.github.com>
2023-02-14 13:13:54 -0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+import { HackerNewsService } from './app.service';
+
 @Component({
-  selector: 'app-root',
+  selector: 'hacker-component',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample';
+
+export class HackerNewsComponent {
+  news = {};
+  constructor(private HackerNewsService: HackerNewsService) {
+    this.HackerNewsService.getNews().subscribe(data => this.news = data);
+  }
 }
