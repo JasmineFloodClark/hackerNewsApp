2b25af52cfcdbc66ef5cd697bbcc1bf8e9a974b5
JasmineFloodClark <125389358+JasmineFloodClark@users.noreply.github.com>
2023-02-14 13:33:31 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HackerNewsComponent } from './app.component';
 import { HackerNewsService } from './app.service';
@@ -13,7 +13,7 @@ import { HackerNewsService } from './app.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpClientModule
   ],
   providers: [HackerNewsService],
   bootstrap: [HackerNewsComponent]
